9456fd4118f3814978b064ea14d5eb425b40eac5
cheporté <cheporte.dev@gmail.com>
2025-04-18 19:36:16 +0300

    🎨 style: Update Home page sections layout

--- client/src/components/Home/MenuPreview/MenuPreview.tsx
+++ client/src/components/Home/MenuPreview/MenuPreview.tsx
@@ -15,17 +15,14 @@ const MenuPreview: React.FC = () => {
                 <div className='menu-item'>
                     <img src={starters[0].imageUrl} alt={starters[0].imageUrl} />
                     <h3>{starters[0].name}</h3>
-                    <p>€{starters[0].price}</p>
                 </div>
                 <div className='menu-item'>
                     <img src={mains[0].imageUrl} alt={mains[0].name} />
                     <h3>{mains[0].name}</h3>
-                    <p>€{mains[0].price}</p>
                 </div>
                 <div className='menu-item'>
                     <img src={desserts[0].imageUrl} alt={desserts[0].name} />
                     <h3>{desserts[0].name}</h3>
-                    <p>€{desserts[0].price}</p>
                 </div>
             </div>
             <Link to='/chez-che-che/menu'><RestaurantButton variant="primary" size="lg">Explore More</RestaurantButton></Link>
